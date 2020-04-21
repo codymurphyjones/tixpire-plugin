7ead36e8ac0af863c78f40baa64e7c7145edcde4
Cody Jones <codymurphyjones@gmail.com>
2020-04-21 04:53:15 -0500

    Initial code repo created and bundler configured

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+import {
+	firebase,
+	storage,
+	firestore,
+	auth
+} from "./firebase"
+
+
+export const firebase;
+export const storage;
+export const firestore;
+export const auth;
+	*/
+	
+import "./config"
+
+let val = document.getElementById("test");
+
+console.log(val);
